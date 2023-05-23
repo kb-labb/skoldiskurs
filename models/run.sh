1223c861ed70d6974e6e7f430787ae25dc0831d2
Hillevi Hägglöf <hilhag@kblabb-l04.kb.local>
2023-05-23 09:39:50 +0200

    modified run file with full corpus file

--- models/run.sh
+++ models/run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# TO DO Fix var names
-OUTPUTDIR=$1 # Is output dir where all files are located.
-CONCATENATEDFILE=$2 # Concatenated file name
-BINFIL=$3
+OUTPUTDIR=$1 # Is output dir where all tokenized and lemmatized files are located.
+CONCATENATEDFILE=$2 # The name of the concatenated file
+BINFIL=$3 # Name of model
 
-echo "starting vectorization..."
-cd $OUTPUTDIR; cat *_export.txt > $CONCATENATEDFILE.txt; rm *_export.txt; cd /home/hilhag/prjs/skoldiskurs/models/
-#python3 train_sentence.py --o $3 --c $1$2.txt --v 300 --a 0.05 #&& \
+
+#cd $OUTPUTDIR; cat *_export.txt > $CONCATENATEDFILE.txt; rm *_export.txt; cd /home/hilhag/prjs/skoldiskurs/models/
+echo "init vectorization..."
+python3 train_sentence.py --o /home/hilhag/prjs/skoldiskurs/vectors/$3 --c $1$2
+echo "Finished vector model."
 #python3 train_sentence.py --o "300v_2021_dn_token_lr0.1.bin" --c "export.xml" --v 300 --a 0.1
